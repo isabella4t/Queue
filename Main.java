62e85d71d49dc9b7eb96f5141ff19b5a26b8cd81
isabella4t <70291996+isabella4t@users.noreply.github.com>
2023-01-13 11:13:41 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    //Queues are First in First Out
+
+    public static void main(String[] args){
+        Queue mq = new Queue();
+        mq.enqueue(7);
+        mq.dequeue();
+    }
+}
